e5cccc51e0f03dfa3462e5950743f1c1fa9df006
Illia Sherbak <illiashcherbak6@gmail.com>
2026-08-28 15:47:32 +0300

    feature: person markup

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,32 @@
 // export const Person = ({ person }) => ();
+export const misha = {
+  name: 'Misha',
+  age: 37,
+  sex: 'm',
+  isMarried: true,
+  partnerName: 'Natasha',
+};
+
+export const Person = ({ person }) => {
+
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is { person.name }</h2>
+      { person.age && (
+        <p className="Person__age">I am { person.age }</p>
+      ) }
+      {
+        person.isMarried ? (
+            <p className="Person__partner">
+              { person.partnerName } is my { person.sex === 'm' ? 'wife' : 'husband' }
+            </p>
+        ) :
+          (
+            <p className="Person__partner">I am not married</p>
+          )
+      }
+
+    </section>
+  );
+}
